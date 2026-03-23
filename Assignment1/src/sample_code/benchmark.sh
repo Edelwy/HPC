c5b9f4faffed055b319f874314c9610e2d297996
Edelwy <nina.mislej1@gmail.com>
2026-03-23 16:23:08 +0100

    improve bash scripts

--- Assignment1/src/sample_code/benchmark.sh
+++ Assignment1/src/sample_code/benchmark.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
-progs=( sample sample-omp )
+#SBATCH --reservation=fri
+#SBATCH --job-name=seam_carving
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=seam_carving.log
+#SBATCH --hint=nomultithread
+
+# Set OpenMP environment variables for thread placement and binding.  
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK 
+
+progs=( seam-carving seam-carving-omp )
 infiles=../test_images
 outfile=results.csv
 repeats=5
 seams=128
 
-echo Compiling...
-for file in ${progs[@]}; do
-	echo ${file}
-#	gcc -O3 --openmp ${file}.c -o ${file} -lm -lnuma
+compile=0
+
+# Optional arguments to make benchmark simpler for compiling and cleanup.
+for arg in "$@"; do
+	if [ "$arg" = "--compile" ]; then
+		compile=1
+	fi
 done
+
+if [ "$compile" = "1" ]; then
+	# Load the numactl module to enable numa library linking.
+	module load numactl
+
+	echo Compiling...
+	for file in "${progs[@]}"; do
+		echo "File: ${file}"
+		gcc -O3 --openmp "${file}.c" -o "${file}.out" -lm -lnuma
+	done
+fi
 
 echo Preparing file...
 echo "Method","Attempt","Image","Time"\;>${outfile}
@@ -21,9 +47,10 @@ for method in ${progs[@]}; do
 		for ((attempt=0; attempt<${repeats}; attempt++)); do
 			echo Running test attempt $attempt with method ${method} on file ${file##*/}
 			echo -n ${method},${attempt},${file##*/},>>${outfile}
-			time=$(./${method} "${file}" "${file}-out" ${seams} | grep "Total time:")
+			time=$(./"${method}.out" "${file}" "${file}-out" ${seams} | grep "Total time:")
 			echo $time
 			echo ${time##Total time:}\;>>${outfile}
 		done
 	done
 done
+
---
+++ Assignment1/src/sample_code/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+prog=$1
+image=$2
+seams=128
+
+compile=0
+
+# Optional arguments to make benchmark simpler for compiling and cleanup.
+for arg in "$@"; do
+	if [ "$arg" = "--compile" ]; then
+		compile=1
+	fi
+done
+
+if [ "$compile" = "1" ]; then
+	# Load the numactl module to enable numa library linking.
+	module load numactl
+	echo Compiling...
+	gcc -O3 --openmp "${prog}.c" -o "${prog}.out" -lm -lnuma
+fi
+
+echo Testing...
+srun  "${prog}.out" "${image}" ./test.png
+
+
